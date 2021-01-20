df175f8be632ccdcc337c8886f929dab09139f8f
Josh Seckman <jseckman90@gmail.com>
2021-01-20 17:23:23 -0500

    checkPalindrome complete

--- codewars.js
+++ codewars.js
@@ -1,19 +1,45 @@
 // Write a program that finds the summation of every number from 1 to num. The number will always be a positive integer greater than 0.
 
-const findSummation = (num) => {
-  let result = 0;
-  for (i = 0; i <= num; i++) {
-    result += i;
-  }
-  return result;
-};
+// const findSummation = (num) => {
+//   let result = 0;
+//   for (i = 0; i <= num; i++) {
+//     result += i;
+//   }
+//   return result;
+// };
 
-findSummation(8);
+// findSummation(8);
 
 // Your goal is to create a function that removes the first and last characters of a string. You're given one parameter, the original string. You don't have to worry with strings with less than two characters.
 
-const removeChar = (str) => {
-  return str.slice(1, -1);
-};
+// const removeChar = (str) => {
+//   return str.slice(1, -1);
+// };
 
-removeChar("hello");
+// removeChar("hello");
+
+// In this kata you will create a function that takes a list of non-negative integers and strings and returns a new list with the strings filtered out.
+
+// const filter_list = (l) => {
+//   for (i = 0; i <= l.length; i++) {
+//     console.log(typeof i);
+//   }
+// };
+
+// filter_list([1, 2, "a", "b"]);
+
+// Given the string, check if it is a palindrome.
+
+// function checkPalindrome(inputString) {
+//   inputString = inputString.split("");
+
+//   stringReverse = inputString.reverse().join();
+//   inputString = inputString.reverse().join();
+
+//   if (inputString === stringReverse) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
+// checkPalindrome("hello");
