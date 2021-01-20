cdbad9778ee35791a5f699778b3a4a91df1953f1
Josh Seckman <jseckman90@gmail.com>
2021-01-19 22:48:40 -0500

    remove first and last character compleete

--- codewars.js
+++ codewars.js
@@ -9,3 +9,11 @@ const findSummation = (num) => {
 };
 
 findSummation(8);
+
+// Your goal is to create a function that removes the first and last characters of a string. You're given one parameter, the original string. You don't have to worry with strings with less than two characters.
+
+const removeChar = (str) => {
+  return str.slice(1, -1);
+};
+
+removeChar("hello");
