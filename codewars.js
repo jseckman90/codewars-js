b14f36bbffd3144d48fe3848a1519022dcc0f790
Josh Seckman <jseckman90@gmail.com>
2021-01-23 22:35:01 -0500

    almostIncreasingSequence tweaked and completed

--- codewars.js
+++ codewars.js
@@ -92,12 +92,12 @@
 // Note: sequence a0, a1, ..., an is considered to be a strictly increasing if a0 < a1 < ... < an. Sequence containing only one element is also considered to be strictly increasing.
 
 function almostIncreasingSequence(sequence) {
-  let invalidItemsCount = [];
+  let nonIncreaseNum = [];
 
   for (i = 1; i < sequence.length; i++) {
     if (sequence[i] <= sequence[i - 1]) {
-      invalidItemsCount.push(sequence[i]);
-      if (invalidItemsCount.length > 1) return false;
+      nonIncreaseNum.push(sequence[i]);
+      if (nonIncreaseNum.length > 1) return false;
       if (sequence[i] <= sequence[i - 2] && sequence[i + 1] <= sequence[i - 1])
         return false;
     }
