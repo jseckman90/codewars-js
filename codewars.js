d9b7897cf041634e6675adf74537d64fa3689a32
Josh Seckman <jseckman90@gmail.com>
2021-02-03 21:58:29 -0500

    compareTriplets complete

--- codewars.js
+++ codewars.js
@@ -182,12 +182,33 @@
 
 // Given an array of integers, find the sum of its elements.
 
-const sumArray = (ar) => {
-  let sum = 0;
-  for (i = 0; i < ar.length; i++) {
-    sum += ar[i];
+// const sumArray = (ar) => {
+//   let sum = 0;
+//   for (i = 0; i < ar.length; i++) {
+//     sum += ar[i];
+//   }
+//   return sum;
+// };
+
+// sumArray([1, 2, 3]);
+
+// Alice and Bob each created one problem for HackerRank. A reviewer rates the two challenges, awarding points on a scale from 1 to 100 for three categories: problem clarity, originality, and difficulty.
+
+// The rating for Alice's challenge is the triplet a = (a[0], a[1], a[2]), and the rating for Bob's challenge is the triplet b = (b[0], b[1], b[2]).
+
+const compareTriplets = (a, b) => {
+  let alice = 0;
+  let bob = 0;
+
+  for (i = 0; i < a.length; i++) {
+    if (a[i] > b[i]) {
+      alice += 1;
+    } else if (b[i] > a[i]) {
+      bob += 1;
+    }
   }
-  return sum;
+  let comp = [alice, bob];
+  console.log(comp);
 };
 
-sumArray([1, 2, 3]);
+compareTriplets([5, 6, 7], [3, 6, 10]);
